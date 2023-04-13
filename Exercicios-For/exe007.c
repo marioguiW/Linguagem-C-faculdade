6898d805b4a46331279cc2d71ae21c7879065c7b
marioguiW <126964100+marioguiW@users.noreply.github.com>
2023-04-13 16:40:01 -0300

    13-04

--- Exercicios-For/exe007.c
+++ Exercicios-For/exe007.c
@@ -1,22 +1,22 @@
 // O número 3025 possui a seguinte característica: 30 + 25 = 55 -> 55*55 = 3025.
 // Fazer um programa para obter todos os números de 4 algarismos com a mesma
-// característica do número 3025. 
+// característica do número 3025. 30 Divisor, 25 Resto da divisão
 
 #include <stdio.h>
 #include <math.h>
 
 int main(){
 
-    int numero;
-
-    printf("Digite um numero de 4 algarismo :\n");
-    scanf("%i",&numero);
-
+    int divisao, restoDivisa,soma;
     
-    for (int i = 0; i < count; i++)
+    for (int i = 1000; i < 10000; i++)
     {
-        /* code */
+        divisao = i / 100;
+        restoDivisa = i % 100;
+        soma = divisao + restoDivisa;
+        if (soma*soma == i)
+        {
+            printf("%i\n", i);
+        }
     }
-    
-
 }
--- Exercicios-For/exe014.c
+++ Exercicios-For/exe014.c
@@ -1,3 +1,22 @@
 // Dado um número n inteiro e positivo, dizemos que n é perfeito se n for igual à
 // soma de seus divisores positivos diferentes de n. Construa um programa em C
 // que verifica se um dado número é perfeito. Ex: 6 é perfeito, pois 1+2+3 = 6. 
+
+#include <stdio.h>
+int main(){
+
+    int numero;
+    
+    printf("Digite um numero para saber se ele e perfeito :\n");
+    scanf("%i", &numero);
+
+    for (int i = numero; i <= numero; i--)
+    {
+        if (numero / i == 0)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
